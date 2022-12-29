6331d14ae05fe2b7905de67093715fb0ffdc6c0c
AbdullahRedoan27 <abdullahredoan8@gmail.com>
2022-12-29 21:38:53 +0600

    my tasks done

--- src/Pages/MyTasks/MyTasks.js
+++ src/Pages/MyTasks/MyTasks.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthProvider';
+import MyTasksCard from './MyTasksCard';
 
 const MyTasks = () => {
+    const {user} = useContext(AuthContext);
+    const [tasks, setTasks] = useState([]);
+    useEffect(()=> {
+        fetch(`http://localhost:5000/mytasks?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setTasks(data)
+        })
+    },[user?.email])
     return (
-        <div>
-            MyTasks
+        <div className='grid lg:grid-cols-3 w-11/12 mx-auto mt-10'>
+            {
+                tasks?.map((task, i) => <MyTasksCard key={i}
+                    task={task}
+                ></MyTasksCard>)
+            }
         </div>
     );
 };
---
+++ src/Pages/MyTasks/MyTasksCard.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function MyTasksCard({task}) {
+  return (
+    <Card sx={{ maxWidth: 345, boxShadow:3, border:1, borderColor: 'grey.500', borderRadius: '15px' }}>
+      <CardMedia
+        component="img"
+        alt="task image"
+        height="140"
+        image={task?.imgURL}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {task?.taskName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {task?.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <b>Added on:</b> {task?.date}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <b>Deadline:</b> {task?.deadline}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Completed</Button>
+        <Button size="small">Delete Task</Button>
+      </CardActions>
+    </Card>
+  );
+}

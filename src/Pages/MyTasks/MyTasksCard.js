d98268fecbe02ce67fa484beed464abaeec7b656
AbdullahRedoan27 <abdullahredoan8@gmail.com>
2022-12-30 21:09:12 +0600

    completed task complete

--- src/Pages/MyTasks/MyTasksCard.js
+++ src/Pages/MyTasks/MyTasksCard.js
@@ -5,8 +5,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import axios, { Axios } from 'axios';
 
 export default function MyTasksCard({task}) {
+    const [completedTask, setCompletedTask] = React.useState();
+    const handleCompleted = (id) => {
+        axios.get(`http://localhost:5000/task?id=${id}`)
+        .then(function(response){
+            console.log(response?.data);
+            setCompletedTask(response?.data)
+        })
+        task = {
+            completedTask
+        }
+        axios.post('http://localhost:5000/completed', {
+            completedTask
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
   return (
     <Card sx={{ maxWidth: 345, boxShadow:3, border:1, borderColor: 'grey.500', borderRadius: '15px' }}>
       <CardMedia
@@ -29,9 +50,10 @@ export default function MyTasksCard({task}) {
           <b>Deadline:</b> {task?.deadline}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Completed</Button>
-        <Button size="small">Delete Task</Button>
+      <CardActions className='flex justify-center'>
+        <Button variant='outlined' onClick={()=>handleCompleted(task?._id)} size="small">Completed</Button>
+        <Button variant='outlined' color="warning" size="small">Edit</Button>
+        <Button variant='outlined' color="error" size="small">Delete Task</Button>
       </CardActions>
     </Card>
   );
